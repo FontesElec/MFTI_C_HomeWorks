420294f28d8c29fcacbf7735ff8cacf1c0b92282
Maxim Chudakov <fontes35@mail.ru>
2025-09-26 13:20:30 +0300

    MFTI Dynamic types home work 13

---
+++ HomeWork13/main.c
@@ -0,0 +1,109 @@
+
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include "temp_api.h"
+
+void test_program(void);
+
+// Массив обработчиков команд
+struct CommandHandler handlers[] = {
+    {"-h",  help_cmd},
+    {"-f",  open_file_cmd},
+    {"-y",  get_statistic_by_year_cmd},
+    {"-m",  get_statistic_by_month_cmd},
+    {"-n",  add_new_entry_cmd},
+    {"-dl", delete_last_entry_cmd},
+    {"-d",  delete_entry_cmd},
+    {"-s",  show_all_data_cmd},
+    {0, unknown_command_cmd} // Маркер конца массива
+};
+
+
+int main(int argc, char* argv[]) {
+
+    //Если при запуске не передан ни один параметр, то запускаем просто демонстрационную программу
+    if(argc == 1){
+        test_program();
+    }
+    else{
+        //Для остальных случаев проходим по аргументам командной строки и последовательно выполняем
+        printf("argc: %d\n", argc);
+        for(int i = 1; i < argc; i++){
+            for(size_t j = 0; j < sizeof(handlers)/sizeof(handlers[0]); j++){
+                //если ничего из списка нет, то дойдём до NULL, напишем сообщение и завершим работу программы
+                if(handlers[j].command_name == NULL){
+                    handlers[j].handler_func(NULL);
+                    return 1;
+                }
+                if (strcmp(argv[i], handlers[j].command_name) == 0) {
+                    //проверяем, является следующий аргумент параметром, или новой командой
+                    if((i + 1 != argc) && (*argv[i+1] == '-')){
+                        handlers[j].handler_func(NULL);
+                    }
+                    else{
+                        handlers[j].handler_func(argv[i+1]);
+                    i++;
+                    }
+                    break;             
+                }            
+            }
+        } 
+        
+    }
+    printf("Press any key to exit\n");
+    getchar();  
+    return 0;
+}
+
+void test_program(void){
+
+    printf("No parameters entered, called demonstration program:\n");
+
+    //Создаём хранилище
+    open_file_cmd(NULL);
+
+    //Загружаем данные   
+    add_new_entry_cmd("2011.2.7;12:23;16");
+    add_new_entry_cmd("2011.2.15;12:23;11");
+    add_new_entry_cmd("2011.3.3;12:23;12");
+    add_new_entry_cmd("2011.3.19;12:23;5");
+    add_new_entry_cmd("2011.4.3;12:23;0");
+    add_new_entry_cmd("2011.4.19;12:23;-10");
+    add_new_entry_cmd("2011.4.24;12:23;-28");
+    add_new_entry_cmd("2011.5.3;12:23;-5");
+    add_new_entry_cmd("2011.5.26;12:23;12");
+    add_new_entry_cmd("2011.6.9;12:23;19");
+    add_new_entry_cmd("2011.3.14;12:23;10");
+    add_new_entry_cmd("2011.6.3;12,:23;16");
+    add_new_entry_cmd("2011.4.13;12:23;-7");
+    add_new_entry_cmd("2011.2.3;12:23;13}");
+    add_new_entry_cmd("2011.5.19;12:23;0");
+    add_new_entry_cmd("2011.4.30;12:23;-13");
+    add_new_entry_cmd("2011.2.9;12:23;17");
+    add_new_entry_cmd("2011.3.26;12:23;0");
+    add_new_entry_cmd("2011.5.13;12:23;-1");
+    add_new_entry_cmd("2011.2.30;12:23;24");
+
+    //выводим статистику
+    get_statistic_by_year_cmd("2011");
+    get_statistic_by_month_cmd("6");
+
+    //выводим записи
+    show_all_data_cmd("ad");
+    show_all_data_cmd("dd");
+    show_all_data_cmd("at");
+    show_all_data_cmd("dt");
+
+    //удал+яем последние записи
+    for(int i = 0; i < 15; i++){
+        delete_last_entry_cmd(NULL);
+    }
+    show_all_data_cmd("ad");
+
+    //удаляем конкретную запись
+    delete_entry_cmd("2011.2.15;12:23");
+    delete_entry_cmd("2011.2.3;12:23");
+    delete_entry_cmd("2011.2.30;12:23");
+    show_all_data_cmd("ad");
+}
---
+++ HomeWork13/storage.h
@@ -0,0 +1,153 @@
+#include <stdint.h>
+
+//--------------------------Defines-----------------------------
+#define STORAGE_SIZE 20
+
+//-------------------------Typedefs-----------------------------
+
+//Cтруктура узла хранилища данных
+typedef struct StorageNode StorageNode;
+
+//Структура заголовка двухсвязного списка хранилища данных
+typedef struct ProtectedStorage ProtectedStorage;
+
+//Базовая структура хранения данных
+typedef struct{                                     
+    uint16_t    year;
+    uint8_t     month;
+    uint8_t     day;
+    uint8_t     hour;
+    uint8_t     min;
+    int8_t      temp;
+} TempData;
+
+/**
+ * Область данных для поиска(указатель на начало хранилища и число записей)
+ * @param data_ptr указатель на StorageNode, с которого начинается поиск
+ * @param num_of_records число записей в списке
+ */
+typedef struct{
+    const StorageNode*       data_ptr;
+    const uint16_t          num_of_records;
+} DataRange;
+
+/**
+ * Формат даты
+ * @param year
+ * @param month
+ * @param day
+*/
+typedef struct{
+    uint16_t year;
+    uint8_t month;
+    uint8_t day;
+} Date;
+
+//Время, для возможности удаления конкретной записи
+typedef struct{
+    uint8_t hour;
+    uint8_t min;
+} Time;
+
+/**
+ * Результат изменения записей в хранилище
+ * @param SUCCESS функция отработала штатно
+ * @param FAIL при выполнении программы возникла проблема
+ */
+typedef enum{
+    SUCSESS,
+    FAIL
+} ProcResult;
+
+/**
+ * Порядок сортировки
+ * @param DESCENDING запускает сортировку по убыванию
+ * @param ASCENDING запускает сортировку по возрастанию
+ */ 
+typedef enum{
+    DESCENDING = -1,
+    ASCENDING = 1
+} SortOrder;
+
+//-------------------------------FunctionPrototypes--------------------------------
+
+//---------------------------------------ФУНКЦИИ ИЗМЕНЕНИЯ ХРАНИЛИЩА------------------------------------------
+/**
+*   Создаёт хранилище фиксированного размера (задано с помощью STORAGE_SIZE). 
+*   @return   Непрозрачный указатель на хранилище
+*/
+ProtectedStorage*  create_storage(void);
+
+/**
+*   Добавляет запись в хранилище.
+*   @param storage  Указатель на заполняемое хранилище
+*   @param data     Данные, которые должны быть добавлены
+*   @return     Результат попытки добавления записи
+*/
+ProcResult push_to_storage(ProtectedStorage* storage, TempData data);
+
+/**
+*   Стирает последнюю запись из хранилища.
+*   @param storage  Указатель на изменяемое хранилище
+*   @return     Результат попытки удаления записи
+*/
+ProcResult pull_from_storage(ProtectedStorage* storage);
+
+/**
+*   Удаляет запись, соответствующую введённым параметрам
+*   @param storage  Указатель на хранилище
+*   @param date     Дата удаляемой записи
+*   @param time     Время удаляемой записи
+*   @return     Результат попытки удаления
+*/
+ProcResult delete_entry(ProtectedStorage* storage, Date date, Time time);
+
+//-------------------------------------------ФУНКЦИИ ПОИСКА--------------------------------------------
+
+/**
+*   Ищет в хранилище область данных, в которой хранятся все записи за требуемый год
+*   @param storage  Указатель на хранилище
+*   @param date     Запрашиваемая дата
+*   @return     Область данных, которая соответствует запрашиваемому году
+*/
+DataRange find_in_year(const ProtectedStorage* storage, Date date);
+
+/**
+*   Ищет в хранилище область данных, в которой хранятся все записи за требуемый месяц
+*   @param storage  Указатель на хранилище
+*   @param date     Запрашиваемая дата
+*   @return     Область данных, которая соответствует запрашиваемому месяцу
+*/
+DataRange find_in_month(const ProtectedStorage* storage, Date date);
+
+//--------------------------------------------ФУНКЦИИ ВЫВОДА--------------------------------------------
+
+/**
+*   Возвращает запрашиваемое значение температуры
+*   @param node_ptr Указатель на узел с данными
+*   @param index    Номер записи, которая необходима
+    @return     Значение температуры.
+*/
+int8_t read_temp_from_storage(const DataRange* node_ptr, uint16_t index);
+
+/**
+*   Возвращает год, за который лежит статистика в хранилище
+*   @param storage Указатель на хранилище
+*   @return Год, указанный в первой записи хранилища 
+*/
+uint16_t get_the_year(const ProtectedStorage* storage);
+
+/**
+*   Выводит на печать всё содержимое массива, по умолчанию сортировка всегда по дате записи
+*   @param storage  Указатель на хранилище
+*   @param ord      Порядок сортировки
+*/
+void print_all_by_date(const ProtectedStorage* storage, SortOrder ord);
+
+/**
+*   Выводит на печать всё содержимое массива, отсортированное по температуре
+*   @param storage  Указатель на хранилище
+*   @param ord      Порядок сортировки
+*/
+void print_all_by_temp(const ProtectedStorage* storage, SortOrder ord); 
+
